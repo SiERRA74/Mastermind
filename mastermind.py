c43043feb1d6cf223a624e697a6edbeec29ed85d
SiERRA74 <75257452+SiERRA74@users.noreply.github.com>
2022-12-25 13:37:07 +0100

    Update mastermind.py

--- mastermind.py
+++ mastermind.py
@@ -1,10 +1,14 @@
 import random
+
 Win = False
+
+
 def chiffremystere():
     intmystere = []
     for i in range(4):
         intmystere.append(random.randint(1, 6))
     return intmystere
+
 
 def play():
     guesses = []
@@ -21,29 +25,33 @@ def play():
         guesses.append(x)
     return guesses
 
-def Mastermind(tries=12,toGuess=chiffremystere()):
+
+def Mastermind(tries=12, toGuess=chiffremystere()):
     Win = False
     if tries == 0:
         return print("You are out of tries, you can retry playing by typing : 'yes'\nin console log")
+
     while Win != True:
         print("you have {} tries to guess the position and color of 4 different balls \n".format(tries))
         print(toGuess)
         Guesses = play()
         print(Guesses)
         common_elements = list(set(toGuess).intersection(Guesses))
+
         perfect = 0
         for i in range(4):
             if int(Guesses[i]) == toGuess[i]:
                 perfect += 1
                 pass
+
         print("You found {} perfect position".format(perfect))
         print("and there's {} ballz with common colors\n \n".format(len(common_elements) - perfect))
-        tries = tries - 1
-        
+
         if perfect == 4:
             Win = True
-            print("You won !\n the right code was actually\n {}".format(toGuess))
+            return print("You won !\n the right code was actually\n {}".format(toGuess))
         else:
-            Mastermind(tries-1,toGuess)
-            
+            Mastermind(tries - 1, toGuess)
+
+
 Mastermind()
